0badf6a252ea1f6a782ab3cbb9c4045392573e4f
RamProsad2 <ramprosad.chandrasutradhar@gmail.com>
2024-08-10 20:59:36 +0300

    add: Binary Tree Node

---
+++ src/main/kotlin/BTNode.kt
@@ -0,0 +1,6 @@
+package org.example
+
+class BTNode<K, V>(var key : K, var value: V,
+                   var left : BTNode<K, V>? = null,
+                   var right : BTNode<K, V>? = null)
+        where K : Comparable<K>, K : Number
