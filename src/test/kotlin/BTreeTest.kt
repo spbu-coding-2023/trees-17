7363f0f87ae960af48a3be7313865ba5c86f4f1f
RamProsad2 <ramprosad.chandrasutradhar@gmail.com>
2024-08-10 21:27:14 +0300

    test: add function

--- src/test/kotlin/BTreeTest.kt
+++ src/test/kotlin/BTreeTest.kt
@@ -45,6 +45,70 @@ class BTreeTest {
     }
 
     @Test
+    fun addOneElement() {
+        val tree = BTree<Int, String>()
+        tree.add(1, "one")
+        assertEquals(1, countNodes(tree))
+        val n = tree.find(1)
+        assertEquals(1, n?.key)
+        assertEquals("one", n?.value)
+    }
+
+    @Test
+    fun addDecreasingSequence() {
+        val tree = BTree<Int, String>()
+        tree.add(5, "five")
+        tree.add(4, "four")
+        tree.add(3, "three")
+        assertEquals(3, countNodes(tree))
+        val n1 = tree.find(3)
+        assertEquals(3, n1?.key)
+        assertEquals("three", n1?.value)
+        val n2 = tree.find(4)
+        assertEquals(4, n2?.key)
+        assertEquals("four", n2?.value)
+        val n3 = tree.find(5)
+        assertEquals(5, n3?.key)
+        assertEquals("five", n3?.value)
+    }
+
+    @Test
+    fun addIncreasingSequence() {
+        val tree = BTree<Int, String>()
+        tree.add(1, "one")
+        tree.add(2, "two")
+        tree.add(3, "three")
+        assertEquals(3, countNodes(tree))
+        val n1 = tree.find(1)
+        assertEquals(1, n1?.key)
+        assertEquals("one", n1?.value)
+        val n2 = tree.find(2)
+        assertEquals(n2?.key, 2)
+        assertEquals("two", n2?.value)
+        val n3 = tree.find(3)
+        assertEquals(3, n3?.key)
+        assertEquals("three", n3?.value)
+    }
+
+    @Test
+    fun addMixedSequence() {
+        val tree = BTree<Int, String>()
+        fillMixed(tree)
+        assertEquals(7, countNodes(tree))
+        for (n in tree)
+            assertEquals(n.key, n.key)
+    }
+
+    @Test
+    fun addDuplicateKeys() {
+        val tree = BTree<Int, String>()
+        tree.add(1, "qwe")
+        assertThrows(Exception::class.java) {
+            tree.add(1, "gdy")
+        }
+    }
+
+    @Test
     fun mainTest() {
         main()
     }
